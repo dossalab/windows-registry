d77ccafd5a03fe7cc42ceb593e021907d9eb4149
Roman Liutko <dossalab@pm.me>
2024-04-30 23:39:56 +0200

    add disable telemetry script

---
+++ disable-telemetry.cmd
@@ -0,0 +1,3 @@
+schtasks /change /disable /tn "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /f
+schtasks /change /disable /tn "Microsoft\Windows\Application Experience\ProgramDataUpdater" /f
+schtasks /change /disable /tn "Microsoft\Windows\Application Experience\StartupAppTask" /f
